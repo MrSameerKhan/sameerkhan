975a63978ea0fb736bf849d8b168bf72e466da51
Sameer Khan <sameerkhanofficial9@gmail.com>
2025-08-25 03:43:29 +0530

    yolo

--- computerVision/yolov8/custom_dataset.py
+++ computerVision/yolov8/custom_dataset.py
@@ -6,7 +6,7 @@ model = YOLO("/Users/sameerkhan/Desktop/sameerkhan/data/cv/yolov8/yolov8n.pt")
 # Train
 model.train(
     data="/Users/sameerkhan/Desktop/sameerkhan/data/cv/yolov8/custom dataset acne/data.yaml",
-    epochs=5,
+    epochs=3,
     batch=16,
     project="/Users/sameerkhan/Desktop/sameerkhan/data/cv/experiments",
     name="yolov8_custom_acne"
